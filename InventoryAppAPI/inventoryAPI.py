77a1eb9f3a96168f0e9e6a05b3ccf66b89ba95c6
SAEED <SAEED@NUAIMI.NET>
2018-06-15 12:47:35 +0200

    enabled GET /product/:id

--- InventoryAppAPI/inventoryAPI.py
+++ InventoryAppAPI/inventoryAPI.py
@@ -3,6 +3,11 @@ import sqlite3
 
 app = Flask(__name__)
 
+#returns a 404 HTTP status code 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
 #returns items from the database as dictioaries
 def dict_factory(cursor,row):
     d = {}
@@ -23,5 +28,26 @@ def api_all():
     cur = conn.cursor()
     all_products = cur.execute('SELECT * FROM data').fetchall()
     return jsonify(all_products)
+
+@app.route('/products/<int:id>')
+def api_product(id):
+    if request.method == 'GET':
+
+        query = "SELECT * FROM data WHERE productid=%s;"%id
+
+        conn = sqlite3.connect('inventory.db')
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+
+        result = cur.execute(query).fetchall()
+        
+        if not result:
+            return page_not_found(404)
+        else:
+            return jsonify(result)
+    else:
+        #add logic for POST
+        pass
+
 
 app.run(port=9214,debug=True)
