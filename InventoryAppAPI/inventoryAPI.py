2345d5714f848aa5a41b662d2f1f7377195f9032
SAEED <SAEED@NUAIMI.NET>
2018-06-18 07:28:34 +0200

    Inventory web app

--- InventoryAppAPI/inventoryAPI.py
+++ InventoryAppAPI/inventoryAPI.py
@@ -29,7 +29,7 @@ def api_all():
     all_products = cur.execute('SELECT * FROM data').fetchall()
     return jsonify(all_products)
 
-@app.route('/products/<int:id>')
+@app.route('/products/<int:id>',methods=['POST','GET'])
 def api_product(id):
     if request.method == 'GET':
 
@@ -46,8 +46,64 @@ def api_product(id):
         else:
             return jsonify(result)
     else:
-        #add logic for POST
-        pass
+        
+        payload = request.get_json()
+        to_filter = []
+
+        #UPDATE data SET name AND
+        query = "UPDATE data SET"
+
+
+        if 'name' in payload:
+            query+= ' name=? AND'
+            to_filter.append(payload['name'])
+
+        if 'size' in payload:
+            query+= ' size=? AND'
+            to_filter.append(payload['size'])
+
+        if 'color' in payload:
+            query+= ' color=? AND'
+            to_filter.append(payload['color'])
+
+        if 'in_stock' in payload:
+            query+= ' instock=? AND'
+            to_filter.append(payload['in_stock'])
+        
+        if ('name' and 'size' and 'color' and 'in_stock') not in payload:
+            return page_not_found(404)
+
+        query = query[:-4] + ' WHERE productid=?'
+
+        to_filter.append(id)
+        print(query)
+        print(to_filter)
+        return "Product has been updated", 200
+
+        # conn = sqlite3.connect('inventory.db')
+        # cur = conn.cursor()
+        # cur.execute(query,to_filter)
+
+
+
+
+
+@app.route('/products/add',methods=['PUT'])
+def api_add():
+    payload = request.get_json()
+
+    pid = payload['product_id']
+    name = payload['name']
+    size = payload['size']
+    color = payload['color']
+    instock = payload['in_stock']
+
+    conn = sqlite3.connect('inventory.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO data VALUES(:productid,:name,:size,:color,:instock)",{'productid':pid,'name':name,'size':size,'color':color,'instock':instock})
+    conn.commit()
+
+    return "Product has been added", 200
 
 
 app.run(port=9214,debug=True)
